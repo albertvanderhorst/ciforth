9ae8b9ff739147243cc37d6194a48855bf09fd0f
albert <albert>
2000-02-18 16:20:27 +0000

    Works properly with the new offset for the reading from boot.

--- genboot.bat
+++ genboot.bat
@@ -1,9 +1,5 @@
-;               HCC FIG generic 8086 FORTH
-; $Id$
-; Copyright (2000): Albert van der Horst, HCC FIG Holland by GNU Public License
-
 REM Generates a bootable disk from a TASM/MASM forth FigForth src in %1
-REM It must be configured as USEDOS and BOOTED .
+REM It must be configured as USEDOS and BOOTED and for the proper disk format.
 
 REM THE FOLLOWING IS TENTATIVE.
 ECHO FLOPPY A: WILL BECOME A FORTH BOOTABLE DISK
@@ -11,25 +7,24 @@ ECHO ALL DATA WILL BE LOST!
 ECHO HIT ^C IF YOU WANT TO STOP.
 PAUSE
 
-Tasm/la/zi %1
+NASM16 %1.asm -o %1.com
 REM Equally possible:
-REM masm %1     
-tlink/v %1
-REM Or tlink/t %1 generating a .com file
+REM Tasm/la/zi %1        or       masm %1
+REM                   plus
+REM tlink/v %1         or       tlink/t %1
 
-REM or NASM16 %1.asm -b %1.com
 echo N > \tmp\fminput.$$$
 echo N >> \tmp\fminput.$$$
 echo N >> \tmp\fminput.$$$
 Format a:/u <\tmp\fminput.$$$
-
-echo N %1.exe> \tmp\dbinput.$$$
+REM BEWARE! This may have to be changed to %1.EXE if not using NASM
+echo N %1.com> \tmp\dbinput.$$$
 echo L >> \tmp\dbinput.$$$
 REM WRITE BOOTSECTOR
-echo W cs:0 0 0 1>> \tmp\dbinput.$$$
+echo W cs:100 0 0 1>> \tmp\dbinput.$$$
 REM WRITE BOOTABLE FILE
-echo N A:FORTH.COM> \tmp\dbinput.$$$
-echo W cs:000>> \tmp\dbinput.$$$
+echo N A:FORTH.COM>> \tmp\dbinput.$$$
+echo W cs:0100>> \tmp\dbinput.$$$
 echo Q>> \tmp\dbinput.$$$
 DEBUG                < \tmp\dbinput.$$$
 
@@ -38,5 +33,3 @@ A:
 ATTRIB +S +R +H FORTH.COM
 C:
 EXIT
-
-
