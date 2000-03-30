f1d15cd286efe716270de591fca84dc45bf36ba6
albert <albert>
2000-03-30 16:02:41 +0000

    content moved to figforth.c

--- tty.h
+++ tty.h
@@ -1,17 +1,1 @@
-/* tty.h */
- 
-#include <termios.h> 
- 
-/* public declarations */ 
-struct ttystate { struct termios tio; }; 
-struct tty      { int fd; struct ttystate org; struct ttystate now; };  
- 
-/* public functions */ 
-void tty_init(int fd,struct tty *ptty);  
-void tty_restore(struct tty *ptty);  
-void tty_keymode(struct tty *ptty); 
-void tty_linemode(struct tty *ptty); 
-void tty_echo(struct tty *ptty); 
-void tty_noecho(struct tty *ptty); 
-int  tty_keyq(struct tty *ptty); 
-int  keyq(int fd);
+
