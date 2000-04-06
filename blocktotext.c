528564376bbf991b7e90b95bf2fb7980bd9e0d63
albert <albert>
2000-04-06 22:06:28 +0000

    Initial revision

---
+++ blocktotext.c
@@ -0,0 +1,19 @@
+/* Program to strip the 8th bits from a file */
+#include <stdio.h>
+
+int atoi();
+
+main(argv, argc)
+char *argv[];
+int argc;
+{
+    int i;
+
+    while(EOF!=(i=getchar()) )
+        if ( EOF==putchar(i&0x7f) )
+        {
+           fprintf(stderr, "Write error in strip8\nProbably disk full\n");
+           exit(1);
+       }
+   exit(0);
+}
