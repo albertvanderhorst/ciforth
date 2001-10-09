5adff4fcb817e8e0e3607b0aa91b9f6a4940eded
albert <albert>
2001-10-09 03:05:23 +0000

    Warning introduced for possible out of sync index lines.

--- toblock.c
+++ toblock.c
@@ -15,24 +15,45 @@ void error(char *message)
     exit(1);
 }
 
-int main( int argc , char **argv ) 
+void check_index(char buffer[])
+{
+    char *pc;
+    static int infoline = 1;
+
+    if ( '(' == buffer[0] ) return;
+
+    for(pc=buffer; *pc; pc++ )
+        if (' ' != *pc && '\n' != *pc )
+        {
+            if ( infoline )
+            {
+                fprintf(stderr, "Possible non-index line in block file \n");
+                infoline = 0 ;
+            }
+            fprintf(stderr, " at block %d:%s", line/16, buffer);
+            return;
+        }
+}
+
+int main( int argc , char **argv )
 {
     int count = 0;
     int i;
      /* One extra for the '\n', one for the '\000', and one to detect
       * oversized lines  */
-    char buffer[CPL+3];  
+    char buffer[CPL+3];
     int ch;
 
     progname = argv[0];
 
     while(NULL != fgets(buffer, CPL+3, stdin))
     {
+        if ( 0 == line%16 ) check_index(buffer);
         line += 1;
         count = strlen(buffer);
         if ( CPL < count ) error("line too long");
         buffer[--count] = '\000';  /* Remove '\n' */
-        for(i=0; i<CPL-1; i++) 
+        for(i=0; i<CPL-1; i++)
             putchar(i<count?buffer[i]:' ');
         putchar('\n');
     }
